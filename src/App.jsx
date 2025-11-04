88eab15fc27e4eea3e57f31dfa2b79a34f2a9af3
Romina Marsico <rominamarsico@users.noreply.github.com>
2025-11-04 18:41:31 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Title } from "./Title"
+import { Card } from "./Card/Card"
+import { InputWithState } from "./Input/InputWithState"
+import { InputWithEffect } from "./Input/InputWithEffect"
+
+function App() {
+  const [count, setCount] = useState(1)
+
+  return (
+    <>
+      <Card link="https://react.dev" image={reactLogo} title="React" />
+      <Card link="https://vite.dev" image={viteLogo} title="Vite" />
+
+      <InputWithState />
+      <InputWithEffect />
+
+      <Title title="hello" />
+      <Title title="Hi and welcome"></Title>
+
+
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
